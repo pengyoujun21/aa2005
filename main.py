86413fa520dcd1b81d8346c931c8d21213ca0055
Aaron <pengyoujun21@163.com>
2021-12-13 16:08:06 +0800

    bb

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm11111')
+    print("0000----")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
